87469ce5932f491c3e1df649589f55ea54ef1b15
Wilkin Novo <w@novo.ws>
2021-11-17 17:08:47 -0500

    feat: Added actions and reducers for users and questions

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,10 +1,12 @@
 import { _getUsers, _getQuestions } from './../_DATA';
-
+import { receiveUsers } from '../actions/users';
+import { receiveQuestions } from '../actions/questions';
 export function getInitialData() {
   return (dispatch) => {
     return Promise.all([_getUsers(), _getQuestions()]).then(
       ([users, questions]) => {
-        //TODO dispatch respective actions
+        dispatch(receiveUsers(users));
+        dispatch(receiveQuestions(questions));
       }
     );
   };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 import loggedUser from './authenticate';
-
+import users from './users';
+import questions from './questions';
 export default combineReducers({
   loggedUser,
+  users,
+  questions,
 });
