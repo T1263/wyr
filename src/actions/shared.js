ffc83b7912603e938acc22eaa13a3c0820d44269
Wilkin Novo <w@novo.ws>
2021-11-17 19:22:32 -0500

    refactor: async and await.

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,12 +2,14 @@ import { _getUsers, _getQuestions } from './../_DATA';
 import { receiveUsers } from '../actions/users';
 import { receiveQuestions } from '../actions/questions';
 export function getInitialData() {
-  return (dispatch) => {
-    return Promise.all([_getUsers(), _getQuestions()]).then(
-      ([users, questions]) => {
-        dispatch(receiveUsers(users));
-        dispatch(receiveQuestions(questions));
-      }
-    );
+  return async (dispatch) => {
+    try {
+      const users = await _getUsers();
+      const questions = await _getQuestions();
+      dispatch(receiveUsers(users));
+      dispatch(receiveQuestions(questions));
+    } catch (error) {
+      console.log(error);
+    }
   };
 }
