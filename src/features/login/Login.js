0e77553873dfda15eee2b914cf6357c90a095414
Wilkin Novo <w@novo.ws>
2021-11-18 15:28:15 -0500

    feat: added Logo

--- src/features/login/Login.js
+++ src/features/login/Login.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Logo from '../logo/Logo';
 import css from './Login.module.css';
 import LoginForm from './_LoginForm';
 
 export default function Login() {
   return (
     <div className={css.login}>
+      <Logo />
       <h1 className={css.head}>Login</h1>
       <LoginForm />
     </div>
---
+++ src/features/logo/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Logo() {
+  return (
+    <div>
+      <h2>WouldYaRather</h2>
+    </div>
+  );
+}
