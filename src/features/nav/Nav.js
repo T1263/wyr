9e12545e5c82da2b4ff32970896ba3079cdbbd6c
Wilkin Novo <w@novo.ws>
2021-11-18 22:37:31 -0500

    feat: active link for current page

--- src/features/nav/Nav.js
+++ src/features/nav/Nav.js
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router';
+import { useNavigate, useLocation } from 'react-router';
+import { Link } from 'react-router-dom';
 import { logOut } from '../login/loginSlice';
 import Logo from '../logo/Logo';
 import css from './Nav.module.css';
@@ -8,16 +9,42 @@ export default function Nav() {
   const currentUser = useSelector(
     ({ users, loggedUser }) => users.users[loggedUser.value]
   );
+  const location = useLocation();
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
   return (
     <div className={css.nav}>
       <div className="container">
         <div className={css.navLeft}>
           <Logo />
           <ul className={css.menu}>
-            <li>New Poll</li>
-            <li>Leaderboard</li>
+            <li>
+              <Link
+                className={location.pathname === '/' ? css.active : ''}
+                to="/"
+              >
+                Start
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={location.pathname === '/new' ? css.active : ''}
+                to="/new"
+              >
+                New Poll
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={
+                  location.pathname === '/leaderboard' ? css.active : ''
+                }
+                to="/leaderboard"
+              >
+                Leaderboard
+              </Link>
+            </li>
           </ul>
         </div>
         <div className={css.navRight}>
