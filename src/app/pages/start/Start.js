91372cba1e0df3b1eb296afa8bfaa52b9c4e9492
Wilkin Novo <w@novo.ws>
2021-11-20 18:05:31 -0500

    feat: Added tabs

--- src/app/pages/start/Start.js
+++ src/app/pages/start/Start.js
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import List from '../../../features/questions/List';
 import css from './Start.module.css';
 import { questionSelector } from '../../../features/selectors';
 
 export default function Start() {
+  const [active, setActive] = useState('Unanswered');
   const { unanswered, answered, users, loading } =
     useSelector(questionSelector);
 
   return (
-    <div>
+    <div className={css.tabs}>
       {loading && <h2 align="center">...loading</h2>}
+      <div className={css.tabsButtons}>
+        <span
+          className={active === 'Unanswered' ? css.active : ''}
+          onClick={() => setActive('Unanswered')}
+        >
+          Unanswered
+        </span>
+        <span
+          className={active === 'Answered' ? css.active : ''}
+          onClick={() => setActive('Answered')}
+        >
+          Answered
+        </span>
+      </div>
       <div className={css.start}>
-        <List
-          name="Unanswered"
-          borderPosition="borderRight"
-          questions={unanswered}
-          users={users}
-        />
-        <List
-          name="Answered"
-          borderPosition="borderLeft"
-          questions={answered}
-          users={users}
-        />
+        {active === 'Unanswered' && (
+          <List
+            name="Unanswered"
+            borderPosition="borderRight"
+            questions={unanswered}
+            users={users}
+          />
+        )}
+        {active === 'Answered' && (
+          <List
+            name="Answered"
+            borderPosition="borderLeft"
+            questions={answered}
+            users={users}
+          />
+        )}
       </div>
     </div>
   );
--- src/features/questions/List.js
+++ src/features/questions/List.js
@@ -2,10 +2,9 @@ import Question from './Question';
 
 import css from './List.module.css';
 
-export default function List({ name, questions, users }) {
+export default function List({ questions, users }) {
   return (
     <div className={css.card}>
-      <h2 className={css.heading}>{name}</h2>
       <ul className={css.list}>
         {Object.keys(questions)
           .map((id) => (
