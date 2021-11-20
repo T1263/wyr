cc943b4396e01be1fdf6ec4dc58a7007b692e3d4
Wilkin Novo <w@novo.ws>
2021-11-19 22:34:36 -0500

    feat: adding question

--- src/app/pages/newPoll/NewPoll.js
+++ src/app/pages/newPoll/NewPoll.js
@@ -1,4 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
+
+import { addQuestion } from '../../../features/questions/questionsSlice';
+import { _saveQuestion } from '../../../_DATA';
+import css from './NewPoll.module.css';
 export default function NewPoll() {
-  return <h1>New Poll Page</h1>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const loggedUser = useSelector((state) => state.loggedUser.value);
+
+  const [option1, setOption1] = useState('');
+  const [option2, setOption2] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch(
+      addQuestion({
+        optionOneText: option1,
+        optionTwoText: option2,
+        author: loggedUser,
+      })
+    );
+
+    navigate('/', { replace: true });
+  };
+  const disabled = () => {
+    if (option1.length <= 5 || option2.length <= 5) return true;
+    return false;
+  };
+  return (
+    <div className={css.newPoll}>
+      <h2> New Poll </h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={option1}
+          placeholder="Enter Option 1"
+          type="text"
+          onChange={({ target }) => setOption1(target.value)}
+        />
+        <small className={css.or}> OR </small>
+        <input
+          value={option2}
+          placeholder="Enter Option 2"
+          type="text"
+          onChange={({ target }) => setOption2(target.value)}
+        />
+        <button disabled={disabled()}>Create</button>
+      </form>
+    </div>
+  );
 }
--- src/features/questions/questionsSlice.js
+++ src/features/questions/questionsSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { _getQuestions } from '../../_DATA';
+import { _getQuestions, _saveQuestion } from '../../_DATA';
 export const fetchQuestions = createAsyncThunk(
   'questions/fetchAll',
   async () => {
@@ -7,17 +7,19 @@ export const fetchQuestions = createAsyncThunk(
   }
 );
 
+export const addQuestion = createAsyncThunk(
+  'questions/add',
+  async (question) => {
+    return await _saveQuestion(question);
+  }
+);
 export const questionsSlice = createSlice({
   name: 'questions',
   initialState: {
     questions: {},
     loading: false,
   },
-  reducers: {
-    setquestions: (state, action) => {
-      state.questions = action.payload;
-    },
-  },
+
   extraReducers: (builder) => {
     builder.addCase(fetchQuestions.pending, (state) => {
       state.loading = true;
@@ -26,8 +28,14 @@ export const questionsSlice = createSlice({
       state.questions = action.payload;
       state.loading = false;
     });
+    builder.addCase(addQuestion.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(addQuestion.fulfilled, (state, action) => {
+      state.questions[action.payload.id] = action.payload;
+      state.loading = false;
+    });
   },
 });
 
-export const { setquestions } = questionsSlice.actions;
 export default questionsSlice.reducer;
