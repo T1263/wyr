7b7561202235475de3262fa3b0a8097e0ea061cb
Wilkin Novo <w@novo.ws>
2021-11-21 20:10:39 -0500

    feat: enpmty unaswered

--- src/features/questions/List.js
+++ src/features/questions/List.js
@@ -4,18 +4,24 @@ import css from './List.module.css';
 
 export default function List({ questions, users }) {
   return (
-    <div className={css.card}>
-      <ul className={css.list}>
-        {Object.keys(questions)
-          .map((id) => (
-            <Question
-              key={id}
-              question={questions[id]}
-              user={users[questions[id].author]}
-            />
-          ))
-          .reverse()}
-      </ul>
-    </div>
+    <>
+      {questions.length > 0 ? (
+        <div className={css.card}>
+          <ul className={css.list}>
+            {Object.keys(questions)
+              .map((id) => (
+                <Question
+                  key={id}
+                  question={questions[id]}
+                  user={users[questions[id].author]}
+                />
+              ))
+              .reverse()}
+          </ul>
+        </div>
+      ) : (
+        <p>...no more.</p>
+      )}
+    </>
   );
 }
