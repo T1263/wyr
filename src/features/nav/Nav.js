99e85f11ba8c27afd4e4d0eb6ca58e271e8f3ce7
Wilkin Novo <w@novo.ws>
2021-11-22 20:29:22 -0500

    fix: there appears to be an issue using useLayoutEffect.
    Reverting back to use useEffect instead.
    
    [v6] Incorrect warning that navigate should be called in a useEffect
    
    See Issue #7460

--- src/features/nav/Nav.js
+++ src/features/nav/Nav.js
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useLocation } from 'react-router';
 import { Link } from 'react-router-dom';
@@ -14,7 +14,7 @@ export default function Nav() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     if (
       (location.pathname === '/' && currentUser === undefined) ||
       (location.pathname === '/add' && currentUser === undefined) ||
