d5a09b051b85b19674b963bedf3140c920249bd4
Wilkin Novo <w@novo.ws>
2021-11-22 21:16:39 -0500

    docs: Pics provided by freepik.com

--- src/features/footer/Footer.js
+++ src/features/footer/Footer.js
@@ -2,5 +2,11 @@ import React from 'react';
 import css from './Footer.module.css';
 export default function Footer() {
   const year = new Date().getFullYear();
-  return <div className={css.footer}>© {year}</div>;
+  return (
+    <div className={css.footer}>
+      © {year}
+      <br />
+      <a href="https://www.freepik.com/">Pics by Freepik.com</a>
+    </div>
+  );
 }
